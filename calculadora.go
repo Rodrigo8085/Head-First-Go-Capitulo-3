10279569b61569b64351af8d89f1530241220bb8
rodrigo <rsantiago643@gmail.com>
2020-01-30 17:38:28 -0600

    sumatoria de litros

--- calculadora.go
+++ calculadora.go
@@ -7,8 +7,14 @@ import (
 var metrosporLitro float64
 
 func main() {
-	metrosporLitro = 10
-	fmt.Printf("%.2f litros son los que se necesitan\n", pintar(4.2, 3.0))
+	var cantidad, total float64
+	cantidad = pintar(4.3, 3.0)
+	fmt.Printf("%0.2f litros son los que se necesitan\n", cantidad)
+	total += cantidad
+	cantidad = pintar(5.3, 3.5)
+	fmt.Printf("%0.2f litros son los que se necesitan\n", cantidad)
+	total += cantidad
+	fmt.Printf("Total: %0.2f litros\n", total)
 }
 
 //la funciones se declaran con el camel Case no pude inicar con un número y debe empezar con una ltra minuscula
@@ -16,5 +22,5 @@ func main() {
 
 func pintar(width float64, height float64) float64 {
 	area := width * height
-	return area / metrosporLitro
+	return area / 10
 }
